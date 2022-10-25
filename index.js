1ca2cbe4294e070a0b8e7144fcbb47985a5f68c0
CLYI <ciaran.lyne@capco.com>
2022-10-25 09:19:41 +0100

    Moved swaggerOptions to seperate config file

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
+const swaggerOptions = require('./swagger.js')
 
 const app = express();
 const port = process.env.PORT || 80;
-
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Hello World API',
-            version: '1.0.0'
-        }
-    },
-    apis: ['index.js']
-};
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
---
+++ swagger.js
@@ -0,0 +1,9 @@
+module.exports = {
+    swaggerDefinition: {
+        info: {
+            title: 'Hello World API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['index.js']
+}
