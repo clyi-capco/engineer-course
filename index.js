a7bb3dd04e738e4a211a96d5b609c8cd05c5a9e0
CLYI <ciaran.lyne@capco.com>
2022-10-25 11:59:08 +0100

    Unit testing for helloWorld APi

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 require('dotenv').config();
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-const swaggerOptions = require('./swagger.js')
+const swaggerOptions = require('./swagger')
 
 const app = express();
+
 const port = process.env.PORT || 80;
+const mongoUrl = process.env.MONGOURL;
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-app.use('/swagger-ui', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+mongoose.connect( mongoUrl ).then(() => {
+    app.use('/swagger-ui', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-/**
- * @swagger
- * /helloWorld:
- *      get:
- *          description: Say hello to the world
- *          responses: 
- *              200:
- *                  description: Success
- */
-app.get('/helloWorld', (req, res) => {
-    res.send('Hello World!');
-    console.log(`responded to request for ${ req.originalUrl }`);
-});
+    /**
+     * @swagger
+     * /helloWorld:
+     *      get:
+     *          description: Say hello to the world
+     *          responses: 
+     *              200:
+     *                  description: Success
+     */
+    app.get('/helloWorld', (req, res) => {
+        res.send('Hello World!');
+        console.log(`responded to request for ${ req.originalUrl }`);
+    });
 
-app.listen(port, () => {
-    console.log(`listening on port ${ port }`);
-});
+    app.listen(port, () => {
+        console.log(`listening on port ${ port }`);
+        app.emit('app_started');
+    });
+
+}).catch(err => console.log(err));
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect;
+const request = require('request');
+const api = require('../index');
+
+const port = process.env.PORT || 80;
+const mongoUrl = process.env.MONGOURL;
+
+before(done => {
+    api.on('app_started', () => {
+        done();
+    });
+});
+
+describe('API', () => {
+    //TODO: const testData = require('test-data.json');
+
+    let baseUrl = `http://localhost:${ port }`;
+
+    describe('Hello World Endpoint', () => {
+        let url = baseUrl + '/helloWorld';
+
+        it("returns status 200", (done) => {
+            request(url, (err, res, body) => {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        it("returns 'Hello World!' in body", (done) => {
+            request(url, (err, res, body) => {
+                expect(body).to.equal('Hello World!');
+                done();
+            });
+        });
+    });
+    
+});
